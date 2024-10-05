4a5554eba74c1e9ba89c15449db3087ee75c36c1
Ikramul Hasan Rakib <hasanrakib3590@gmail.com>
2024-10-05 21:28:44 +0600

    commit

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -19,18 +19,18 @@ const NewsCard = ({ item }: NewsCardProps) => {
           height={500}
           className="mb-5 md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200"
         ></Image>
-      </Link>
 
-      <div>
-        <h2 className="text-xl font-semibold my-3">
-          {item?.title.substring(0, 65)}
-        </h2>
-        <p className="mb-4">{item?.description.substring(0, 100)}...</p>
-        <Link href={`/news/${item._id}`}></Link>
-        <Button className="hover:bg-gray-200 hover:text-black hover:border">
-          Read More
-        </Button>
-      </div>
+        <div>
+          <h2 className="text-xl font-semibold my-3">
+            {item?.title.substring(0, 65)}
+          </h2>
+          <p className="mb-4">{item?.description.substring(0, 100)}...</p>
+          <Link href={`/news/${item._id}`}></Link>
+          <Button className="hover:bg-gray-200 hover:text-black hover:border">
+            Read More
+          </Button>
+        </div>
+      </Link>
     </div>
   );
 };
